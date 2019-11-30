306efd49be31f7d068e5d63bd65be7dbcf53c83e
take447 <48678403+take447@users.noreply.github.com>
2019-11-30 23:59:03 +0900

    Update liff_test.js

--- liff_test.js
+++ liff_test.js
@@ -1,10 +1,14 @@
 window.onload = function (e) {
 
     // コールバックを使用する方法
-    liff.init({ liffId: "1653591503-97y46Wm0" }, initializeApp,initializeApp);
+    liff.init({ liffId: "1653591503-97y46Wm0" }, initializeApp,er);
 
 }
 
 function initializeApp(data) {
     document.getElementById('uid').value = data.context.userId;
+}
+
+function er(data) {
+    document.getElementById('uid').value = '失敗';
 }
