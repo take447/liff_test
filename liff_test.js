0aa9b2bec4460c53a33c8bfe8543f6765acd5a72
take447 <48678403+take447@users.noreply.github.com>
2019-12-01 22:03:43 +0900

    Update liff_test.js

--- liff_test.js
+++ liff_test.js
@@ -3,6 +3,9 @@ window.onload = function (e) {
     // コールバックを使用する方法
     liff.init({ liffId: "1653591503-97y46Wm0" }, initializeApp,er);
 
+    //とりあえずボタンを押したらLIFFを閉じてみる
+    document.getElementById("btn").addEventListener("click", liff.closeWindow);
+
 }
 
 function initializeApp(data) {
